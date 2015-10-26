aa3b5a6fad8c5b5c02f91b01cd0d161d16ef42b2
dada-sss <ashwinarvind12@gmail.com>
2015-10-26 00:41:41 -0400

    Update calender.c

--- calender.c
+++ calender.c
@@ -1,70 +1,140 @@
+
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"project.h"
+#define student 1
+#define admin 2
 
-#define TRUE    1
-#define FALSE   0
-
-int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-char *months[]=
-{
-	" ",
-	"\n\n\nJanuary",
-	"\n\n\nFebruary",
-	"\n\n\nMarch",
-	"\n\n\nApril",
-	"\n\n\nMay",
-	"\n\n\nJune",
-	"\n\n\nJuly",
-	"\n\n\nAugust",
-	"\n\n\nSeptember",
-	"\n\n\nOctober",
-	"\n\n\nNovember",
-	"\n\n\nDecember"
+struct book{
+	int id;
+	char name[50];
+	char author[50];
+	int quantity;
+	int rackno;
 };
 
-//takes a number(year) as input from user
-int inputyear(void)
-{
-	int year;
-	
-	printf("Please enter a year: ");
-	scanf("%d", &year);
-	return year;
-}
-//this function checks if the given year is a leap year or not
-int checkleapyear(int year)
-{
-	if(year% 4 == FALSE && year%100 != FALSE || year%400 == FALSE)
-	{
-		days_in_month[2] = 29;
-		return TRUE;
+struct issue {
+	int member_id;
+	char book_name[50],member_name[50];
+	int i_day,i_mon,i_yr;
+	int r_day,r_mon,r_yr;
+
+};
+
+struct member {
+	int id;
+	char name[50];
+
+};
+
+/*Function to add members to library.
+ *The user(the "Librarian" only!) has to input the member ID and name of student.
+ *It gets stored in file*/
+void addmember() {
+	int choice,flush;
+	struct member mem;
+	char name[50];
+	int id;
+	FILE *fm;
+
+	fm = fopen("member.txt","a");
+	if(!fm) {
+		printf("error\n");
+		mem.id=0;
 	}
-	else
-	{
-		days_in_month[2] = 28;
-		return FALSE;
+
+	do {
+			printf("Enter member id :");
+			scanf("%d",&mem.id);
+			
+			printf("Enter member Name:");
+			fflush(stdin);
+			scanf("%s",mem.name);
+
+
+		printf("Save the record and go to menu? 1.Yes 2.No \n");
+		scanf("%d",&choice);
+		if(choice == 1){
+			fwrite(&mem,sizeof(mem),1,fm);
+			fclose(fm);
+			menu();
+			return;
+		}
+	}while(choice == 1);
+	if(choice == 2) {
+		menu();
 	}
 }
 
-void calendar(int year, int daycode)
-{
-	int month, day;
-	for ( month = 1; month <= 12; month++ )
-	{
-		printf("%s", months[month]);
-		printf("\n\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n" );
-		
-		
-		
+/*Shows all the records of member- Their ID and names*/
+void viewmember() {
+	FILE *fm;
+	struct member read;
+	fm = fopen("member.txt","r");
+
+	while (fread (&read, sizeof(struct member),1, fm)){
+		printf ("ID = %d   Name = %s \n",read.id,read.name);
+	}
+	fclose(fm);
+	menu();
 }
 
+/*Function to add books to library.
+ *The user(the "Librarian" only!) has to input the book ID,name of book,author,copies and rack number.
+ *It gets stored in file*/
 
-int main(void)
-{
-	int year, day, leapyear;
+void addbooks(){
+	int choice,flush;
+	struct book bk;
+	char name[50], author[50];
+	int quantity, rackno;
+	FILE *fp;
+	fp = fopen("book.txt","a");
+
+	if(!fp){
+		printf("error\n");		
+		bk.id=0;
+	}
+
+	do {
+			printf("Enter Book id :");
+			scanf("%d",&bk.id);
+		
+			printf("Enter Book Name:");
+			scanf("%d",&flush);
+			fflush(stdin);
+			fgets(name,50,stdin);
+			strcpy(bk.name,name);
+
+			printf("Enter Book Author:");
+			fgets(author,50,stdin);
+			strcpy(bk.author,author);
+
+
+
+			printf("Enter number of copies:");
+			scanf("%d",&quantity);
+			bk.quantity=quantity;
+		
+			printf("enter rack number :");
+			scanf("%d",&rackno);
+			bk.rackno=rackno;
+
+		
+		
+		printf("Save the record and go to menu? 1.Yes 2.No \n");
+		scanf("%d",&choice);
+		if(choice == 1){
+			fwrite(&bk,sizeof(bk),1,fp);
+			fclose(fp);
+			menu();
+			return;
+		}
+	}while(choice == 1);
+	if(choice == 2) {
+		menu();
+	}
 	
-	year = inputyear();
-	days = daycode(year);//yet to be writtern
-	checkleapyear(year);
-	calendar(year, day);
-	printf("\n");
+
 }
